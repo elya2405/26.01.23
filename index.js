8c6c6f97503239929547d8eab1766e71df2b8ff2
kanybekova eleonora <eelya250@gmail.com>
2023-01-26 14:57:27 +0600

    task with sum

--- index.js
+++ index.js
@@ -29,3 +29,12 @@ function myFriend(name) {
 myFriend("Ajara");
 myFriend("Nestan");
 myFriend("Tina");
+
+function printSum(a, b) {
+  let sum = a + b;
+  console.log("The sum of " + a + " and " + b + " is " + sum);
+}
+
+printSum(5, 10);
+printSum(-5, 0);
+printSum(100, 125);
