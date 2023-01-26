395b2459778156d5a4ffc8500b8cd33af5a7e5de
kanybekova eleonora <eelya250@gmail.com>
2023-01-26 14:41:10 +0600

    function

---
+++ index.js
@@ -0,0 +1,16 @@
+function sayHello() {
+  console.log("Hello");
+}
+function sayBye() {
+  console.log("Bye");
+}
+
+sayHello();
+sayHello();
+
+function sayHello(name) {
+  console.log("Hello " + name);
+}
+
+sayHello("Bakyt");
+sayHello("Dactan");
