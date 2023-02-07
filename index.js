a33284d825de9a241de889cb44929570b0c8fc0c
kanybekova eleonora <eelya250@gmail.com>
2023-02-07 14:35:47 +0600

    tasks to repeat

--- index.js
+++ index.js
@@ -59,3 +59,67 @@ function printDivide(x, y) {
 }
 
 printDivide(20, 5);
+
+function isDivibleBy(a, b) {
+  let remainder = a % b;
+  if (remainder === 0) {
+    console.log(a + " is divisible by " + b);
+  } else if (remainder === 1) {
+    console.log(a + " is not divisible by " + b);
+  }
+}
+
+isDivibleBy(5, 2);
+isDivibleBy(4, 2);
+
+function isEven(a) {
+  let number = 0;
+  if (a % 2 === 0) {
+    console.log(a + " is even");
+  } else if (a % 2 === 1) {
+    console.log(a + " is not even");
+  } else {
+    console.log(a + " is not number");
+  }
+}
+
+isEven(5);
+isEven(2);
+isEven("asd");
+isEven(0);
+
+// ex 1
+isPositive(-5);
+isPositive(5);
+
+function isPositive(a) {
+  let number = a;
+  if (a > 0) {
+    console.log(a + " is positive");
+  } else if (a < 0) {
+    console.log(a + " is not positive");
+  } else {
+    console.log(a + " is not number");
+  }
+}
+
+// ex 2
+usdToKgs(400);
+
+function usdToKgs(x) {
+  let dollar = x;
+  let som = x * 80;
+  console.log(x + " is " + som);
+}
+
+// ex 3 
+circleArea(5); // circle with diameter of 5 cm is 51cm2
+
+function circleArea(d) {
+  let p = 3.14;
+  let r = d / 2;
+  let area = r ** 2 * p;
+  console.log("circle with diameter of " + d + area) 
+  }
+
+
